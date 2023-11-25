763b5f42d0ae8429fd9760cc40f74055e1206b16
Дарина Григорьева <darinadmitrievna2411@gmail.com>
2023-11-25 11:49:17 +0700

    написан класс для картинки

---
+++ cursovaya/MyPicture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cursovaya
+{
+    internal class MyPicture
+    {
+        public string Name { get; set; }
+        public string Img { get; set; }
+        public string Categ { get; set; }
+        public MyPicture() { }
+        public MyPicture(string name, string img)
+        {
+            Name = name;
+            Img = img;
+            Categ = "";
+        }
+        public MyPicture(string name, string img, string categ)
+        {
+            Name = name;
+            Img = img;
+            Categ = categ;
+        }
+
+    }
+}
